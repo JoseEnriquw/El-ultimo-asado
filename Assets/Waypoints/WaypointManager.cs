fda42de961b95eb345f8981cfc684cba321e1a0f
ElMarche <marcelojpelleriti@gmail.com>
2024-10-29 02:24:29 -0300

    Agregado y prueba 2 animaciones
    30/10/2024
    Character Remy, Megan, Josh, Jody, Brian:
    -Se adicionan 2 animaciones a c/u: Muerte cayendo hacia adelante y hacia atrás.
    -Se testean en escena de prueba "Marce" con WaypointManager (script) y un bool para disparar muerte.
    -Si el personaje está caminando muerte cayendo hacia adelante.
    -Si el personaje está idle muerte cayendo hacia atrás.
    -Animación Walking cambia velocidad si se cambia la velocidad de movimiento de personaje en WaypointManager (script).

--- Assets/Waypoints/WaypointManager.cs
+++ Assets/Waypoints/WaypointManager.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 
 public class WaypointManager : MonoBehaviour
 {
+    // la lista de wayPoints asignados desde el editor
     [SerializeField] private List<Transform> wayPoints = new List<Transform>();
+
     public bool isMoving;
     public int wayPointIndex;
     [SerializeField] private float moveSpeed;
     [SerializeField] private float rotationSpeed;
+    
+    // para aplicar movimiento en bucle
     [SerializeField] bool isLoop;
+
+    // para aplicar movimiento aleatorio entre los waypoints
     [SerializeField] bool isRandom;
+    
+    // solo para probar la animacion de muerte
+    [SerializeField] bool isDead;
+
+    // almacenaremos posicion antes de manipular transform
     private Vector3 previousPosition;
+
+    // el animator para administrar la maquina de estados
     Animator animator;
     
 
@@ -24,10 +38,24 @@ public class WaypointManager : MonoBehaviour
         //isMoving = true;
         animator = GetComponent<Animator>();
         previousPosition = new Vector3(transform.position.x, 0.0f, transform.position.z);
+        
+        // al comienzo esta vivo
+        isDead = false;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        Animate();
+
+        if(!isDead)
+        {
+            WaypointMovement();
+        }
+                
+    }
+
+    private void Animate()
     {
         // obtenemos el bool que decide la transicion a walking
         bool isWalking = animator.GetBool("isWalking");
@@ -39,23 +67,34 @@ public class WaypointManager : MonoBehaviour
         // almacenamos en un float si hubo desplazamiento del character
         float movingDifference = Vector3.Distance(previousPosition, transform.position);
         //Debug.Log(movingDifference);
-        
+
         // chequeamos si se movio mas que una cierta distancia
         isMoving = movingDifference >= 0.0005f;
         //Debug.Log(isMoving.ToString());
-        
-        // animacion desactivada y se esta moviendo?
-        if (!isWalking && isMoving)
-        {
-            // activa animacion
-            animator.SetBool("isWalking", true);
-        }
-        // animacion activada  y ya no se esta desplazando el character
-        if (isWalking && !isMoving)
-        {
-            animator.SetBool("isWalking", false);
-        }
 
+        if (!isDead)
+        {
+            animator.SetFloat("speedMultiplier", moveSpeed);
+            // animacion desactivada y se esta moviendo?
+            if (!isWalking && isMoving)
+            {
+                // activa animacion
+                animator.SetBool("isWalking", true);
+            }
+            // animacion activada  y ya no se esta desplazando el character
+            if (isWalking && !isMoving)
+            {
+                animator.SetBool("isWalking", false);
+            }
+        } else
+        {
+            animator.SetBool("isDead", true);
+        }
+        
+    }
+
+    private void WaypointMovement()
+    {
         previousPosition = new Vector3(transform.position.x, 0.0f, transform.position.z);
         // ingresa siempre que tengamos mas de 1 waypoint al comienzo
         if (wayPointIndex < wayPoints.Count)
@@ -69,10 +108,10 @@ public class WaypointManager : MonoBehaviour
             // calcula el el angulo de rotacion y aplica rotacion con LERP
             Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
             transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
-            
+
             // calcula la distancia hacia siguiente waypoint
             float distance = Vector3.Distance(transform.position, wayPoints[wayPointIndex].position);
-            
+
             // llego al siguiente waypoint?
             if (distance <= 0.05f)
             {
@@ -81,7 +120,8 @@ public class WaypointManager : MonoBehaviour
                 {
                     // asigna waypoint aleatorio
                     wayPointIndex = Random.Range(0, wayPoints.Count);
-                }else
+                }
+                else
                 {
                     // incrementa el contador de waypoint
                     wayPointIndex++;
@@ -93,9 +133,9 @@ public class WaypointManager : MonoBehaviour
                         wayPointIndex = 0;
                     }
                 }
-                
+
             }
-            
+
         }
     }
 }
