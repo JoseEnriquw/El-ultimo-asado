ac1e5a8969fdbc3384f7d4038cba64d4fd1baa60
Regina laurentino <reginalaurentino54@gmail.com>
2024-11-03 03:34:32 -0300

    tarea de buscar medical kit
    tarea de buscar medical kit

--- Assets/Scripts/PickObject.cs
+++ Assets/Scripts/PickObject.cs
@@ -82,71 +82,169 @@ public class PickObject : MonoBehaviour
         Gizmos.DrawLine(raycastOrigin.position, raycastOrigin.position + Vector3.forward * checkDistance);
     }
     #endregion
+    #region MANEJO DE TRIGGERS
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.CompareTag("ObjetoPickeable") && (maskCajones != (maskCajones | (1 << other.gameObject.layer))))
+        ultimoreconocido = false; 
+       
+        GameObject otherObject = other.gameObject;
+        string tag = otherObject.tag;
+        switch (tag)
+        {
+            case "ObjetoPickeable":
+                HandleObjetoPickeable(otherObject);
+                break;
+            case "ItemInventory":
+                HandleItemInventory(otherObject);
+                break;
+            case "MedicalKit":
+                HandleMedicalKit(otherObject);
+                break;
+            case "Cuadros":
+                HandleCuadros(otherObject);
+                break;
+            default:
+                break;
+        }      
+    }    
+
+    private void HandleObjetoPickeable(GameObject otherObject)
+    {
+        if (maskCajones != (maskCajones | (1 << otherObject.layer)))
         {
             ultimoreconocido = true;
             if (Input.GetKey(KeyCode.E) && pickedObject == null)
             {
-                other.GetComponent<Rigidbody>().useGravity = false;
-                other.GetComponent<Rigidbody>().isKinematic = true;
-                other.transform.position = hanPoint.transform.position;
-                other.gameObject.transform.SetParent(hanPoint.gameObject.transform);
-                pickedObject = other.gameObject;
+                Rigidbody rb = otherObject.GetComponent<Rigidbody>();
+                rb.useGravity = false;
+                rb.isKinematic = true;
+                otherObject.transform.position = hanPoint.transform.position;
+                otherObject.transform.SetParent(hanPoint.transform);
+                pickedObject = otherObject;
             }
-        }
-        else if (other.gameObject.CompareTag("ItemInventory"))
-        {
-            ultimoreconocido = true;
-            if (Input.GetKey(KeyCode.E))
-            {
-                GameObject Itempickedup = other.gameObject;
-
-                ItemInventory item = Itempickedup.GetComponent<ItemInventory>();
-
-                _inventory.AddItem(Itempickedup, item);
-            }
-        }
-        else if (other.gameObject.CompareTag("ObjetoPickeable") && (maskCajones == (maskCajones | (1 << other.gameObject.layer))))
-        {
-            ultimoreconocido = true;
-            other.GetComponent<OpenandClose>().AbreCierra();
-            if (Input.GetKey(KeyCode.E))
-            {
-                
-            }
-
-
-        }
-        else if (other.gameObject.CompareTag("MedicalKit"))
-        {
-            Text textComponent = other.GetComponent<Text>();
-            if (!Busquedabotiquin)
-            {
-                Busquedabotiquin = true;
-                healthKitCounter.IncrementarContador(true);
-            }
-            else
-            {
-                ultimoreconocido = true;
-                if (Input.GetKey(KeyCode.E))
-                {
-                    GameObject Itempickedup = other.gameObject;
-
-                    ItemInventory item = Itempickedup.GetComponent<ItemInventory>();
-
-                    _inventory.AddItem(Itempickedup, item);
-                    healthKitCounter.IncrementarContador();
-                }
-            }
-            
         }
         else
         {
-            ultimoreconocido = false;
+            ultimoreconocido = true;
+            otherObject.GetComponent<OpenandClose>().AbreCierra();
         }
     }
+
+    private void HandleItemInventory(GameObject otherObject)
+    {
+        ultimoreconocido = true;
+        if (Input.GetKey(KeyCode.E))
+        {
+            GameObject Itempickedup = otherObject.gameObject;
+            ItemInventory item = otherObject.GetComponent<ItemInventory>();
+            _inventory.AddItem(otherObject, item);
+        }
+    }    
+
+    private void HandleMedicalKit(GameObject otherObject)
+    {
+        Text textComponent = otherObject.GetComponent<Text>();
+        InteractableObject interactable = otherObject.GetComponent<InteractableObject>();
+        if (!Busquedabotiquin &&  interactable != null && interactable.interactionID==11)
+        {
+            Busquedabotiquin = true;
+            healthKitCounter.IncrementarContador(true);
+        }
+        else if(Busquedabotiquin)
+        {
+            ultimoreconocido = true;
+            if (Input.GetKey(KeyCode.E))
+            {
+                GameObject Itempickedup = otherObject.gameObject;
+                ItemInventory item = otherObject.GetComponent<ItemInventory>();
+                _inventory.AddItem(otherObject, item);
+                healthKitCounter.IncrementarContador();
+            }
+        }
+    }
+
+    private void HandleCuadros(GameObject otherObject)
+    {
+        Rigidbody cuadroRb = otherObject.GetComponent<Rigidbody>();
+        if (cuadroRb != null)
+        {
+            //REVISAR
+            cuadroRb.isKinematic = false; // Deshabilitar isKinematic para que se aplique la física
+            cuadroRb.AddForce(Vector3.down * 5f, ForceMode.Impulse); // Aplicar fuerza hacia abajo
+            Collider cuadroCollider = otherObject.GetComponent<Collider>();
+            if (cuadroCollider != null)
+            {
+                cuadroCollider.enabled = false; // Desactivar el colisionador para que no bloquee el paso
+            }
+        }
+    }
+
+    #endregion
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("ObjetoPickeable") && (maskCajones != (maskCajones | (1 << other.gameObject.layer))))
+    //    {
+    //        ultimoreconocido = true;
+    //        if (Input.GetKey(KeyCode.E) && pickedObject == null)
+    //        {
+    //            other.GetComponent<Rigidbody>().useGravity = false;
+    //            other.GetComponent<Rigidbody>().isKinematic = true;
+    //            other.transform.position = hanPoint.transform.position;
+    //            other.gameObject.transform.SetParent(hanPoint.gameObject.transform);
+    //            pickedObject = other.gameObject;
+    //        }
+    //    }
+    //    else if (other.gameObject.CompareTag("ItemInventory"))
+    //    {
+    //        ultimoreconocido = true;
+    //        if (Input.GetKey(KeyCode.E))
+    //        {
+    //            GameObject Itempickedup = other.gameObject;
+
+    //            ItemInventory item = Itempickedup.GetComponent<ItemInventory>();
+
+    //            _inventory.AddItem(Itempickedup, item);
+    //        }
+    //    }
+    //    else if (other.gameObject.CompareTag("ObjetoPickeable") && (maskCajones == (maskCajones | (1 << other.gameObject.layer))))
+    //    {
+    //        ultimoreconocido = true;
+    //        other.GetComponent<OpenandClose>().AbreCierra();
+    //        if (Input.GetKey(KeyCode.E))
+    //        {
+
+    //        }
+
+
+    //    }
+    //    else if (other.gameObject.CompareTag("MedicalKit"))
+    //    {
+    //        Text textComponent = other.GetComponent<Text>();
+    //        if (!Busquedabotiquin)
+    //        {
+    //            Busquedabotiquin = true;
+    //            healthKitCounter.IncrementarContador(true);
+    //        }
+    //        else if(Busquedabotiquin)
+    //        {
+    //            ultimoreconocido = true;
+    //            if (Input.GetKey(KeyCode.E))
+    //            {
+    //                GameObject Itempickedup = other.gameObject;
+
+    //                ItemInventory item = Itempickedup.GetComponent<ItemInventory>();
+
+    //                _inventory.AddItem(Itempickedup, item);
+    //                healthKitCounter.IncrementarContador();
+    //            }
+    //        }
+
+    //    }
+    //    else
+    //    {
+    //        ultimoreconocido = false;
+    //    }
+    //}
     #region OBJETO 
     public void SetPickedObject(GameObject item)
     {
