2026f9b8eb6dfad1ebc0f271d612f89d2fcb3e19
Regina laurentino <reginalaurentino54@gmail.com>
2024-11-02 21:20:22 -0300

    agrandar hand
    agrandar hand

---
+++ Assets/Scripts/OpenandClose.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenandClose : MonoBehaviour
+{
+    public void ActivarObjeto()
+    {
+        Destroy(gameObject);
+    }
+}
