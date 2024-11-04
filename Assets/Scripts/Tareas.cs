57e437d9c452305bb8c5f92bb5d8cbceb7c963ae
Regina laurentino <reginalaurentino54@gmail.com>
2024-11-03 21:19:11 -0300

    correciones
    correciones

---
+++ Assets/Scripts/Tareas.cs
@@ -0,0 +1,48 @@
+using Assets.Scripts.GameManager;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tareas : MonoBehaviour
+{
+    private int Scenenumber;
+    void Start()
+    {
+        Scenenumber= GameManager.GetGameManager().GetSceneNumber();
+        AsingTask(Scenenumber);
+    }
+
+    private void AsingTask(int scenenumber)
+    {
+        Text textComponent = this.GetComponent<Text>();
+        var text="";
+        switch (scenenumber)
+        {
+            case 0: text = "Buscar leña, fosforos y carbon";
+                textComponent.UpdateTextBasedOnInteraction(true, text, false);
+                break;
+            case 1:
+                text = "Buscar la linterna";
+                textComponent.UpdateTextBasedOnInteraction(true, text, false);
+                break;
+            case 2:
+                text = "Buscar el botiquin";
+                textComponent.UpdateTextBasedOnInteraction(true, text, false);
+                break;
+            case 3:
+                text = "Encuentra la llave y escapa";
+                textComponent.UpdateTextBasedOnInteraction(true, text, false);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
